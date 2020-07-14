07fa8cecc30461b1b9bf63313d82cb71cfb32924
Felix Lamas <felamas@gmail.com>
2020-07-13 23:15:18 -0300

    primer funcion del helper

---
+++ ManejoDeArchivos/ManejoDeArchivos/Program.cs
@@ -0,0 +1,32 @@
+﻿using Helpers;
+using System;
+using System.IO;
+
+namespace ManejoDeArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreDelArchivo = "destino";
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(nombreDelArchivo);
+        }
+    }
+}
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion (string nombre)
+        {
+            if (!File.Exists(nombre))
+            {
+                FileStream detino = new FileStream(nombre, FileMode.Create);
+            }
+            else
+            {
+                Console.WriteLine("el archivo existe");
+            }
+        }
+        
+}
